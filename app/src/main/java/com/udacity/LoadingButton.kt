5e178ddc7f8b2baccc80dd11335edf81680c656a
Matheus Lima <matheuslima9412@gmail.com>
2020-12-06 16:04:44 -0300

    improves code

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -6,28 +6,51 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
-import androidx.core.content.ContextCompat
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import kotlin.properties.Delegates
 
 
 class LoadingButton @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 0
-    private var heightSize = 0
-    private var originalBg = ContextCompat.getColor(context, R.color.colorPrimary)
-    private val downloadBg = ContextCompat.getColor(context, R.color.colorPrimaryDark)
-    private val downloadText = "Download"
-    private val downloadingText = "We are downloading"
-    private var textToShow = downloadText
     var progress: Int = 0
     var isLoading: Boolean = false
+
+    private var widthSize = 0
+    private var heightSize = 0
+    private var originalBg = getColorFromAttr(R.attr.loading_button_idle_background)
+    private val downloadBg = getColorFromAttr(R.attr.loading_button_background)
+    private val downloadText = context.getString(R.string.download_idle)
+    private val downloadingText = context.getString(R.string.donwloading)
+    private var textToShow = downloadText
     private var colorAnimation = ValueAnimator()
     private val rectF = RectF()
     private var rectLoading = Rect()
     private val rectText = Rect()
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var circleX = 0F
+    private var circleY = 0F
+
+    private val loadingPaint = Paint().apply {
+        color = downloadBg
+    }
+    private val circlePaint = Paint().apply {
+        color = getColorFromAttr(R.attr.loading_circle_color)
+    }
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        color = getColorFromAttr(R.attr.loading_button_text)
+    }
+
+    init {
+        isClickable = true
+    }
+
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { _, _, new ->
         if(new is ButtonState.Loading) {
             isLoading = true
             textToShow = downloadingText
@@ -54,27 +77,8 @@ class LoadingButton @JvmOverloads constructor(
         invalidate()
     }
 
-    init {
-        isClickable = true
-    }
-
     private fun calculateAngle(): Float {
         return (progress * 360 / widthSize).toFloat()
-    }
-
-    private val loadingPaint = Paint().apply {
-        color = downloadBg
-    }
-    private var circleX = 0F
-    private var circleY = 0F
-    private val circlePaint = Paint().apply {
-        color = Color.YELLOW
-    }
-    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 55.0f
-        color = Color.WHITE
     }
 
     fun setState(buttonState: ButtonState) {
@@ -89,7 +93,6 @@ class LoadingButton @JvmOverloads constructor(
         colorAnimation.duration = duration // milliseconds
         setupAnimListener()
         colorAnimation.start()
-
     }
 
     private fun setupAnimListener() {
@@ -115,6 +118,15 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
+    @ColorInt
+    private fun getColorFromAttr(
+            @AttrRes attrColor: Int
+    ): Int {
+        val typedValue = TypedValue()
+        val theme = context.theme
+        theme.resolveAttribute(attrColor, typedValue, true)
+        return typedValue.data
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -123,7 +135,10 @@ class LoadingButton @JvmOverloads constructor(
         rectLoading.set(0, heightSize, progress, 0)
         canvas?.drawRect(rectLoading, loadingPaint)
 
-        canvas?.drawText(textToShow, (widthSize / 2).toFloat(), (heightSize / 2) - ((textPaint.descent() + textPaint.ascent()) / 2), textPaint)
+        canvas?.drawText(textToShow,
+                (widthSize / 2).toFloat(),
+                (heightSize / 2) - ((textPaint.descent() + textPaint.ascent()) / 2),
+                textPaint)
         if(isLoading) {
             rectF.set(circleX,circleY,circleX+80,circleY+80)
             canvas?.drawArc(rectF,0F,calculateAngle(),true, circlePaint)
